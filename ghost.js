5515d251a0bdb597389b5db1986a3143bff2ad6d
jonghwa-kim <zcbm4321@gmail.com>
2023-04-29 21:00:17 +0900

    dynamic loading

--- ghost.js
+++ ghost.js
@@ -1,6 +1,25 @@
 
-function changeByJS() {
-    let x = document.getElementsByClassName("loading")[0];
-    x.innerText = "complete";
-    x.style.color = "red";
+const parseResponse = (nodes) => {
+    let randIdx = Math.floor(Math.random()*nodes.length);
+    console.log(nodes);
+    console.log(nodes[0]);
+    console.log(nodes[0].ip);
+    return nodes[randIdx];
 }
+
+const loadNodesHtml = (node) => {
+    return `http://${node.ip.Ip}:${node.ip.Port}`;
+}
+
+fetch("http://ghostnetroot.com:58080/nodes")
+    .then((response) =>response.json())
+//.then((data)=> new Map(Object.entries(data)))
+    .then(parseResponse)
+    .then(loadNodesHtml)
+    .then((addr)=>fetch(addr)
+        .then((response) =>response.text())
+        .then((data)=> {
+            let r = document.getElementsByClassName("root")[0];
+            r.insertAdjacentHTML("afterbegin", data);
+        }));
+
