158cd292d36395adfb80eebe2c71cb6891f2fa36
zcbm4321 <zcbm4321@gmail.com>
2023-09-18 07:07:47 +0900

    new upload

---
+++ static/hons/uploadhon.js
@@ -0,0 +1,12 @@
+export class UploadHon {
+    constructor(blockStore, session) {
+        this.blockStore = blockStore;
+        this.session = session;
+        this.m_session = session;
+    }
+    Run(masterAddr) {
+        return true;
+    }
+    Release() { }
+}
+//# sourceMappingURL=uploadhon.js.map
