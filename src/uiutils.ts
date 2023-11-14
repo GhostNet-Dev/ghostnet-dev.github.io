13b1f021114883503bf72a029d79462ec57e9dd1
zcbm4321 <zcbm4321@gmail.com>
2023-11-14 11:19:02 +0900

    alert update

--- src/uiutils.ts
+++ src/uiutils.ts
@@ -1,31 +1,54 @@
 import { includeContentHTML, includeHTML, parseResponse, loadNodesHtml } from "./ghostweb.js";
 
-if (location.protocol != 'http:') {
-    alert("warning");
-} else {
-    addEventListener("load", () =>
-        fetch("http://ghostnetroot.com:58080/nodes")
-            .then((response) => response.json())
-            .then(parseResponse)
-            .then(loadNodesHtml)
-            .then((url) => includeContentHTML(url))
-            .then(() => {
-                const navbar = document.querySelector("navbar");
-                const navbarHeight = navbar?.getBoundingClientRect().height || 40;
-                addEventListener("scroll", () => {
-                    if (window.scrollY > navbarHeight) {
-                        navbar?.classList.remove("navbar-dark");
-                        navbar?.classList.remove("bg-dark");
-                        navbar?.classList.add("navbar-transition");
-                    } else {
-                        navbar?.classList.remove("navbar-transition");
-                        navbar?.classList.add("navbar-dark");
-                        navbar?.classList.add("bg-dark");
-                    }
+const tag = document.getElementById("contents");
+if (tag != null) {
+    if (location.protocol != 'http:') {
+        tag.innerHTML = `
+            <div class="card">
+                <div class="card-header"> 
+                    https 를 지원하지 않습니다. 
+                </div>
+                <div class="card-body">
+                    <a href="http://ghostwebservice.com"> <strong class="me-auto">ghostwebservice.com</strong> </a>
+                </div>
+            </div>
+        `;
+    } else {
+        addEventListener("load", () =>
+            fetch("http://ghostnetroot.com:58080/nodes")
+                .then((response) => response.json())
+                .then(parseResponse)
+                .then(loadNodesHtml)
+                .then((url) => includeContentHTML(url))
+                .then(() => {
+                    const navbar = document.querySelector("navbar");
+                    const navbarHeight = navbar?.getBoundingClientRect().height || 40;
+                    addEventListener("scroll", () => {
+                        if (window.scrollY > navbarHeight) {
+                            navbar?.classList.remove("navbar-dark");
+                            navbar?.classList.remove("bg-dark");
+                            navbar?.classList.add("navbar-transition");
+                        } else {
+                            navbar?.classList.remove("navbar-transition");
+                            navbar?.classList.add("navbar-dark");
+                            navbar?.classList.add("bg-dark");
+                        }
+                    })
                 })
-            }));
+                .catch(() => {
+                    tag.innerHTML = `
+            <div class="card">
+                <div class="card-header"> 
+                Network Down
+                </div>
+                <div class="card-body">
+                    사용가능한 Node가 존재하지 않습니다. 
+                </div>
+            </div>
+        `;
+                }));
+    }
 }
-
 
 
 includeHTML("header", "navbar.html");
--- static/uiutils.js
+++ static/uiutils.js
@@ -1,29 +1,53 @@
 import { includeContentHTML, includeHTML, parseResponse, loadNodesHtml } from "./ghostweb.js";
-if (location.protocol != 'http:') {
-    alert("warning");
-}
-else {
-    addEventListener("load", () => fetch("http://ghostnetroot.com:58080/nodes")
-        .then((response) => response.json())
-        .then(parseResponse)
-        .then(loadNodesHtml)
-        .then((url) => includeContentHTML(url))
-        .then(() => {
-        const navbar = document.querySelector("navbar");
-        const navbarHeight = (navbar === null || navbar === void 0 ? void 0 : navbar.getBoundingClientRect().height) || 40;
-        addEventListener("scroll", () => {
-            if (window.scrollY > navbarHeight) {
-                navbar === null || navbar === void 0 ? void 0 : navbar.classList.remove("navbar-dark");
-                navbar === null || navbar === void 0 ? void 0 : navbar.classList.remove("bg-dark");
-                navbar === null || navbar === void 0 ? void 0 : navbar.classList.add("navbar-transition");
-            }
-            else {
-                navbar === null || navbar === void 0 ? void 0 : navbar.classList.remove("navbar-transition");
-                navbar === null || navbar === void 0 ? void 0 : navbar.classList.add("navbar-dark");
-                navbar === null || navbar === void 0 ? void 0 : navbar.classList.add("bg-dark");
-            }
-        });
-    }));
+const tag = document.getElementById("contents");
+if (tag != null) {
+    if (location.protocol != 'http:') {
+        tag.innerHTML = `
+            <div class="card">
+                <div class="card-header"> 
+                    https 를 지원하지 않습니다. 
+                </div>
+                <div class="card-body">
+                    <a href="http://ghostwebservice.com"> <strong class="me-auto">ghostwebservice.com</strong> </a>
+                </div>
+            </div>
+        `;
+    }
+    else {
+        addEventListener("load", () => fetch("http://ghostnetroot.com:58080/nodes")
+            .then((response) => response.json())
+            .then(parseResponse)
+            .then(loadNodesHtml)
+            .then((url) => includeContentHTML(url))
+            .then(() => {
+            const navbar = document.querySelector("navbar");
+            const navbarHeight = (navbar === null || navbar === void 0 ? void 0 : navbar.getBoundingClientRect().height) || 40;
+            addEventListener("scroll", () => {
+                if (window.scrollY > navbarHeight) {
+                    navbar === null || navbar === void 0 ? void 0 : navbar.classList.remove("navbar-dark");
+                    navbar === null || navbar === void 0 ? void 0 : navbar.classList.remove("bg-dark");
+                    navbar === null || navbar === void 0 ? void 0 : navbar.classList.add("navbar-transition");
+                }
+                else {
+                    navbar === null || navbar === void 0 ? void 0 : navbar.classList.remove("navbar-transition");
+                    navbar === null || navbar === void 0 ? void 0 : navbar.classList.add("navbar-dark");
+                    navbar === null || navbar === void 0 ? void 0 : navbar.classList.add("bg-dark");
+                }
+            });
+        })
+            .catch(() => {
+            tag.innerHTML = `
+            <div class="card">
+                <div class="card-header"> 
+                Network Down
+                </div>
+                <div class="card-body">
+                    사용가능한 Node가 존재하지 않습니다. 
+                </div>
+            </div>
+        `;
+        }));
+    }
 }
 includeHTML("header", "navbar.html");
 includeHTML("footer", "foot.html");
